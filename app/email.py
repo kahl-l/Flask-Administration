2ba7e59a2694fdbfa3c3d7529e097bdba3bed718
Kahl <ludovic.kahl@gmail.com>
2018-02-06 18:13:31 +0100

    Undo

--- app/email.py
+++ app/email.py
@@ -6,7 +6,7 @@ def send_email(subject, sender, recipients, text_body, html_body):
 	msg = Message(subject, sender=sender, recipients=recipients)
 	msg.body = text_body
 	msg.html = html_body
-	# mail.send(msg)
+	mail.send(msg)
 
 def send_password_reset_email(user):
 	token = user.get_reset_password_token()
