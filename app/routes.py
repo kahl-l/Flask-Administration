763e56d919dd2e38e87eb5c292942b273aefcf67
Kahl <ludovic.kahl@gmail.com>
2018-02-07 12:36:29 +0100

    Correct user posts

--- app/routes.py
+++ app/routes.py
@@ -66,11 +66,7 @@ def register():
 @login_required
 def user(username):
 	user = User.query.filter_by(username=username).first_or_404()
-	posts = [
-		{'author': user, 'body': 'Test post #1'},
-		{'author': user, 'body': 'Test post #2'}
-	]
-	return render_template('user.html', user=user, posts=posts)
+	return render_template('user.html', user=user, posts=user.posts)
 
 @app.before_request
 def before_request():
