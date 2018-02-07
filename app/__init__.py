d80f94d2f6c254d549007027bfb02ab29cf64a27
Kahl <ludovic.kahl@gmail.com>
2018-02-07 20:05:38 +0100

    Error blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,9 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from app.errors import bp as errors_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.register_blueprint(errors_bp)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
@@ -22,6 +20,9 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
 
 from app import routes, models
 
