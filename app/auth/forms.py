9d49379ecae0052c5d944a071013b2d8844bf669
Kahl <ludovic.kahl@gmail.com>
2018-02-15 17:26:27 +0100

    Disable registration

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -9,19 +9,19 @@ class LoginForm(FlaskForm):
 	remember_me = BooleanField('Remember Me')
 	submit 		= SubmitField('Sign in')
 
-class RegistrationForm(FlaskForm):
-	first_name 	= StringField('First Name', validators=[DataRequired()])
-	last_name 	= StringField('Last Name', validators=[DataRequired()])
-	email 		= StringField('Email', validators=[DataRequired(), Email()])
-	password 	= PasswordField('Password', validators=[DataRequired()])
-	password2 	= PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-	title	 	= StringField('Title', validators=[DataRequired()])
-	submit 		= SubmitField('Register')
+# class RegistrationForm(FlaskForm):
+# 	first_name 	= StringField('First Name', validators=[DataRequired()])
+# 	last_name 	= StringField('Last Name', validators=[DataRequired()])
+# 	email 		= StringField('Email', validators=[DataRequired(), Email()])
+# 	password 	= PasswordField('Password', validators=[DataRequired()])
+# 	password2 	= PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+# 	title	 	= StringField('Title', validators=[DataRequired()])
+# 	submit 		= SubmitField('Register')
 
-	def validate_email(self, email):
-		user = User.query.filter_by(email=email.data).first()
-		if user is not None:
-			raise ValidationError('Please use a different email address.')
+# 	def validate_email(self, email):
+# 		user = User.query.filter_by(email=email.data).first()
+# 		if user is not None:
+# 			raise ValidationError('Please use a different email address.')
 
 class ResetPasswordRequestForm(FlaskForm):
 	email 	= StringField('Email', validators=[DataRequired(), Email()])
--- app/auth/routes.py
+++ app/auth/routes.py
@@ -29,20 +29,20 @@ def logout():
 	logout_user()
 	return redirect(url_for('main.index'))
 
-@bp.route('/register', methods=['GET', 'POST'])
-def register():
-	if current_user.is_authenticated:
-		return redirect(url_for('main.index'))
-	form = RegistrationForm()
-	if form.validate_on_submit():
-		user = User(first_name=form.first_name.data, last_name=form.last_name.data,
-			email=form.email.data, title=form.title.data)
-		user.set_password(form.password.data)
-		db.session.add(user)
-		db.session.commit()
-		flash('Congratulations, you are now a registered user!', 'success')
-		return redirect(url_for('auth.login'))
-	return render_template('auth/register.html', title='Register', form=form)
+# @bp.route('/register', methods=['GET', 'POST'])
+# def register():
+# 	if current_user.is_authenticated:
+# 		return redirect(url_for('main.index'))
+# 	form = RegistrationForm()
+# 	if form.validate_on_submit():
+# 		user = User(first_name=form.first_name.data, last_name=form.last_name.data,
+# 			email=form.email.data, title=form.title.data)
+# 		user.set_password(form.password.data)
+# 		db.session.add(user)
+# 		db.session.commit()
+# 		flash('Congratulations, you are now a registered user!', 'success')
+# 		return redirect(url_for('auth.login'))
+# 	return render_template('auth/register.html', title='Register', form=form)
 
 @bp.route('/reset_password_request', methods=['GET', 'POST'])
 def reset_password_request():
