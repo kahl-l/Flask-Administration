f8650fc462d20cba222b9757e9c78a6931c9a7be
Kahl <ludovic.kahl@gmail.com>
2018-02-16 17:38:40 +0100

    Add flash

--- app/article/routes.py
+++ app/article/routes.py
@@ -1,4 +1,4 @@
-from flask 				import render_template
+from flask 				import render_template, flash
 from flask_login 		import login_required, current_user
 from app				import db
 from app.article 		import bp
