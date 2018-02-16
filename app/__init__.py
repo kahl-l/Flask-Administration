835bf784b8d4b3493826855567602329e11af752
Kahl <ludovic.kahl@gmail.com>
2018-02-16 16:20:31 +0100

    register article

--- app/__init__.py
+++ app/__init__.py
@@ -29,13 +29,16 @@ def create_app(config_class=Config):
 	bootstrap.init_app(app)
 	moment.init_app(app)
 
-	from app.errors import bp as errors_bp
+	from app.errors 	import bp as errors_bp
 	app.register_blueprint(errors_bp)
 
-	from app.user 	import bp as user_bp
+	from app.user 		import bp as user_bp
 	app.register_blueprint(user_bp, url_prefix='/user')
 
-	from app.main 	import bp as main_bp
+	from app.article 	import bp as article_bp
+	app.register_blueprint(article_bp, url_prefix='/article')
+
+	from app.main 		import bp as main_bp
 	app.register_blueprint(main_bp)
 
 	if not app.debug:
