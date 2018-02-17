75e68c87784674562a1649c7d324d1e3f400dbbf
Kahl <ludovic.kahl@gmail.com>
2018-02-17 21:09:21 +0100

    Correct edition

--- app/article/routes.py
+++ app/article/routes.py
@@ -1,4 +1,4 @@
-from flask 				import render_template, flash, redirect, url_for
+from flask 				import render_template, flash, redirect, url_for, request
 from flask_login 		import login_required, current_user
 from app				import db
 from app.article 		import bp
