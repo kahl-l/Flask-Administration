94ddf2d11c0768b20f4d909446b51fb7a4a24608
Kahl <ludovic.kahl@gmail.com>
2018-02-19 15:18:28 +0100

    Add import

--- app/article/forms.py
+++ app/article/forms.py
@@ -1,3 +1,4 @@
+from flask 				import flash
 from flask_wtf 			import FlaskForm
 from flask_wtf.file 	import FileField, FileRequired, FileAllowed
 from wtforms 			import StringField, TextAreaField, SubmitField
