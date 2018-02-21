b627f110f0be2ffa8af11cd452c504f573527316
Kahl <ludovic.kahl@gmail.com>
2018-02-21 14:58:58 +0100

    Add image

--- administration.py
+++ administration.py
@@ -1,8 +1,8 @@
 from app 		import create_app, db
-from app.models import User, Article
+from app.models import User, Article, Image
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-	return {'db': db, 'User': User, 'Article': Article}
+	return {'db': db, 'User': User, 'Article': Article, 'Image': Image}
